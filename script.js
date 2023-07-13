fd8dbaadf304bed3f180a3f54efb2e069bba1472
lpch20 <lpch20@hotmail.com>
2023-07-13 20:46:46 -0300

    add cart and counter

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const add = document.getElementById("add");
 const rest = document.getElementById("rest");
+let counterProduct = document.getElementById("counterProduct");
+let bodyCart = document.getElementById("bodyCart");
+let productNumber = document.getElementById("productNumber");
+let checkout = document.getElementById("checkout");
 
 add.addEventListener("click", () => {
-  let counterProduct = document.getElementById("counterProduct");
   let valueNumber = parseInt(counterProduct.value);
 
   if (isNaN(valueNumber)) {
@@ -15,4 +18,17 @@ rest.addEventListener("click", () => {
   let valueNumber = parseInt(counterProduct.value);
 
   counterProduct.value = valueNumber - 1;
+});
+
+cart.addEventListener("click", () => {
+  if (bodyCart.style.display === "none") {
+    bodyCart.style.display = "flex";
+  } else {
+    bodyCart.style.display = "none";
+  }
+});
+
+checkout.addEventListener("click", (e) => {
+  e.preventDefault(e);
+  productNumber.value = counterProduct.value;
 });
